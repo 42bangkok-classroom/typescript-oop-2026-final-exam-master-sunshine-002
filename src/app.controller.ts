07b144fe9cae0e466b8a59bce1873e96883777ab
sunshine-002 <sunshine.17923@gmail.com>
2026-03-25 11:49:01 +0700

    oiiiii

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiResponse } from './interfaces/response.interface';
+
 
 @Controller()
 export class AppController {
